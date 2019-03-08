9a39a52b3f2870ccbb1bcbcebe7d9993a79d6d5b
GalileoCap <40899339+GalileoCap@users.noreply.github.com>
2019-03-08 20:50:36 -0300

    LIMPIEZA

--- gali-gases_con_z.py
+++ gali-gases_con_z.py
@@ -17,7 +17,7 @@ Created on Wed Mar  6 09:44:26 2019
 import random
 import numpy as np
 
-def random_start(n, x_size, y_size, z_size): #A: s= side, d= deer, l= lions
+def random_start(n, x_size, y_size, z_size): #U: Creates random positions for each molecule #A: Stolen from my Prey vs Predator simmulator
     r= np.repeat(" ", x_size*y_size*z_size).reshape(x_size, y_size, z_size)
     x_pos= []
     y_pos= []
@@ -28,27 +28,63 @@ def random_start(n, x_size, y_size, z_size): #A: s= side, d= deer, l= lions
         pos= (random.randint(0, x_size-1), random.randint(0, y_size-1), random.randint(0, z_size-1))
         if r[pos] == " ":
             r[pos]= "P"
-            i+= 1
             x_pos.append(pos[0])
             y_pos.append(pos[1])
             z_pos.append(pos[2])
+            i+= 1
 
     print('Posiciones iniciales = \n', r)
     return x_pos, y_pos, z_pos
 
-def simmulate(n=20, r=1, dt=1, m=1, vol= 50): #A: n=cantidad de particulas; r= cantidad de frames; dt= dela tiempo en segundos; m= masa de las particulas; vol= volumen en cm3
-    K= 1E-1 #1.38064852E-23
+def random_vel(n, vmin, vmax): #U: Crea n velocidades al azar (la velocidad en xyz es la misma)
+    r= []
+    for i in range(n):
+        r.append(random.randint(vmin, vmax)**(1./3.))
+        
+    return r, r, r
+
+def random_mass(n, which): #U: Elije al azar entre las masas qe hayas elegido
+    r= []
+    for i in range(n):
+        r.append(which[random.randint(0, len(which)-1)])
+
+    return r
+    
+def calc_force(i, pos_x, pos_y, pos_z, K): #U: Calculates the force "felt" by this molecule
+    Fx, Fy, Fz= [0] * 3 #A: Each force starts with a value of zero
+    for j in range(len(pos_x)):
+        if j != i:
+            distance= (pos_x[i]-pos_x[j])**2 + (pos_y[i]-pos_y[j])**2 + (pos_z[i]-pos_z[j])**2 #A: Sacada de la consigna
+            Fx+= (pos_x[i]-pos_x[j])/(distance**3) #A: Sacada de la consigna
+            Fy+= (pos_y[i]-pos_y[j])/(distance**3)
+            Fz+= (pos_z[i]-pos_z[j])/(distance**3)
+    Fx= 4*K*Fx #A: Terminando la formula
+    Fy= 4*K*Fy
+    Fz= 4*K*Fz
+    
+    return Fx, Fy, Fz
+
+def check_sides(pos, v, side): #U: Checks if the molecule bounced with a side
+    if pos > side:
+        v= -v
+        pos-= 2*(pos-side)
+    if pos < side:
+        v= -v
+        pos-= 2*pos
+
+    return pos, v
+def simmulate(n=20, r=1, dt=1, which=[1], vol= 50, vmin=0, vmax=0): 
+    #A: n=cantidad de particulas; r= cantidad de frames; dt= dela tiempo en segundos; m= masa de las particulas; which= masas posibles; vol= volumen en cm3; vmin/vmax= rango para las velocidades iniciales
+    K= 1E-1 #A: I use this value because it works for me, but the actual constant K = 1.38064852E-23
     
     pos_x, pos_y, pos_z= random_start(n, vol, vol, vol)
-    vx= [0] * n #A: Una lista de n ceros #XXX: Empezar con velocidad
-    vy= [0] * n
-    vz= [0] * n
-    #print('INICIAL \n', pos_x, '\n', pos_y)
+    vx, vy, vz= random_vel(n, vmin, vmax)
+    m= random_mass(n, which)
     
     salida= open("salida.xyz", "w")
 
     for l in range(r):
-        print(n, file=salida)
+        print(n, file=salida) #A: For making the ".xyz" file and storing the coordinates
         print(" ", file=salida)
         for jj in range(n):
             print("6", pos_x[jj], pos_y[jj], pos_z[jj], "0", file=salida)
@@ -56,70 +92,31 @@ def simmulate(n=20, r=1, dt=1, m=1, vol= 50): #A: n=cantidad de particulas; r= c
         paso_x= [] #A: Para guardar las posiciones y despues reemplazar las pos
         paso_y= []
         paso_z= []
-        paso_vx= []
-        paso_vy= []
-        paso_vz= []
+        
         for i in range(n):
-            esta_Fx= 0
-            esta_Fy= 0
-            esta_Fz= 0
-            for j in range(n):
-                if i != j:
-                    #print("Comparando", i, j)
-                    esta_D= (pos_x[i]-pos_x[j])**2 + (pos_y[i]-pos_y[j])**2 + (pos_z[i]-pos_z[j])**2 #A: Sacada de la consigna
-                    esta_Fx+= (pos_x[i]-pos_x[j])/(esta_D**3) #A: Sacada de la consigna
-                    esta_Fy+= (pos_y[i]-pos_y[j])/(esta_D**3)
-                    esta_Fz+= (pos_z[i]-pos_z[j])/(esta_D**3)
-            esta_Fx= 4*K*esta_Fx #A: Terminando la formula
-            esta_Fy= 4*K*esta_Fy
-            esta_Fz= 4*K*esta_Fz
+            Fx, Fy, Fz= calc_force(i, pos_x, pos_y, pos_z, K)
             
-            esta_vx= vx[i]+(esta_Fx/m)*dt #A: Sacado de la consigna
-            esta_vy= vy[i]+(esta_Fy/m)*dt
-            esta_vz= vz[i]+(esta_Fz/m)*dt
+            vx[i]+= (Fx/m)*dt #A: See README
+            vy[i]+= (Fy/m)*dt
+            vz[i]+= (Fz/m)*dt
             
-            esta_x= pos_x[i]+esta_vx*dt
-            esta_y= pos_y[i]+esta_vy*dt
-            esta_z= pos_z[i]+esta_vz*dt
+            esta_x= pos_x[i]+vx[i]*dt
+            esta_y= pos_y[i]+vy[i]*dt
+            esta_z= pos_z[i]+vz[i]*dt
             
-            if esta_x > x_size: #A: Si se pasa de largo
-                print("ERR", esta_x)
-                esta_vx= -esta_vx
-                esta_x-= 2*(esta_x-x_size)
-            if esta_x < 0:
-                esta_vx= -esta_vx
-                esta_x-= 2*esta_x
-                
-            if esta_y > y_size:
-                esta_vy= -esta_vy
-                esta_y-= 2*(esta_y-y_size)
-            if esta_y < 0:
-                esta_vy= -esta_vy
-                esta_y= -2*esta_y
-            
-            if esta_z > z_size:
-                esta_vz= -esta_vz
-                esta_z-= 2*(esta_z-z_size)
-            if esta_z < 0:
-                esta_vz= -esta_vz
-                esta_z-= 2*esta_z
+            esta_x, vx[i]= check_sides(esta_x, vx[i], vol)
+            esta_y, vy[i]= check_sides(esta_y, vy[i], vol)
+            esta_z, vz[i]= check_sides(esta_z, vz[i], vol)
                 
             paso_x.append(esta_x)
             paso_y.append(esta_y)
             paso_z.append(esta_z)
-            
-            paso_vx.append(esta_vx)
-            paso_vy.append(esta_vy)
-            paso_vz.append(esta_vz)
-        #XXX: Guardar las posiciones nuevas
+
         #print('PASO Nº', l, '\n', paso_x, '\n', paso_y)
         pos_x= paso_x.copy() #A: Actualizo las posiciones
         pos_y= paso_y.copy()
         pos_z= paso_z.copy()
-        vx= paso_vx.copy()
-        vy= paso_vy.copy()
-        vz= paso_vz.copy()
         
     print("END")
-simmulate(250, 1000)
+#simmulate(250, 1000)
 #simmulate(30, 100, x_size=100, y_size=100)
